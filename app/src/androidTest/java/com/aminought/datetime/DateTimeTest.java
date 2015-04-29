80b87bd966f1aff7637e5b7d712b4011e8c422ec
aminought <aminought@gmail.com>
2015-04-29 15:18:41 +0500

    Added simple test

---
+++ app/src/androidTest/java/com/aminought/datetime/DateTimeTest.java
@@ -0,0 +1,18 @@
+package com.aminought.datetime;
+
+import junit.framework.TestCase;
+
+public class DateTimeTest extends TestCase {
+
+    public void testDiff() throws Exception {
+        DateTime dt1 = new DateTime(2015, 4, 29, 12, 45);
+        DateTime dt2 = new DateTime(2015, 4, 29, 10, 45);
+        DateTime resActual1 = dt1.diff(dt2);
+        DateTime resActual2 = dt2.diff(dt1);
+        assertEquals(resActual1.getMinute(), resActual2.getMinute());
+        assertEquals(resActual1.getHour(), resActual2.getHour());
+        assertEquals(resActual1.getDay(), resActual2.getDay());
+        assertEquals(resActual1.getMonth(), resActual2.getMonth());
+        assertEquals(resActual1.getYear(), resActual2.getYear());
+    }
+}
